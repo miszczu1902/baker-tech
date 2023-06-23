cbf1468f136ccd801484b18790b9861d9d4b1621
Bartosz Miszczak <bartoszmiszczak1@gmail.com>
2023-06-23 12:34:03 +0000

    Project/init backend

---
+++ backend/src/main/java/pl/p/lodz/it/baker/tech/bakertech/BakertechApplication.java
@@ -0,0 +1,13 @@
+package pl.p.lodz.it.baker.tech.bakertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakertechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakertechApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/p/lodz/it/baker/tech/bakertech/BakertechApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.p.lodz.it.baker.tech.bakertech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakertechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
